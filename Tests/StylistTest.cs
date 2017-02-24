ebc2fdb025e1aa4e7e42d07c6482bba39cd23bcd
keely glenn <glenj392@newschool.edu>
2017-02-24 10:32:28 -0800

    add test method for get all and save

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -27,6 +27,40 @@ namespace HairSalon
             }
         }
 
+        // test that all stylist will be returned in list
+        [Fact]
+        public void GetAll_ReturnAllStylists_list()
+        {
+            // arrange
+            Stylist stylist1 = new Stylist("Kendra", "1-Cl", "Mens hair", 1);
+            Stylist stylist2 = new Stylist("Maddy", "1-Cl", "Mens hair", 2);
+            stylist1.Save();
+            stylist2.Save();
+
+            // act
+            List<Stylist> testStylistList = new List<Stylist> {stylist1, stylist2};
+            List<Stylist> resultStylistList = Stylist.GetAll();
+
+            // assert
+            Assert.Equal(testStylistList, resultStylistList);
+        }
+
+        // this will test the save method
+        [Fact]
+        public void Save_TestIfSaved_saved()
+        {
+            //Arrange
+            Stylist stylist1 = new Stylist("Kendra", "1-Cl", "Mens hair", 1);
+            stylist1.Save();
+
+            // Act
+            List<Stylist> testStylistList = new List<Stylist> {stylist1};
+            List<Stylist> resultStylistList = Stylist.GetAll();
+
+            //Assert
+            Assert.Equal(testStylistList, resultStylistList);
+        }
+
         // prevent repeats of tests or multiple test interferience
         public void Dispose()
        {
