2c44d19433b9c82474adb3465d0e96fdda1c08ac
keely glenn <glenj392@newschool.edu>
2017-02-24 10:39:32 -0800

    add method for get all and save for stylists

--- Objects/Stylist.cs
+++ Objects/Stylist.cs
@@ -72,6 +72,78 @@ namespace HairSalon
             return _id;
         }
 
+        // get all method for Stylist
+        public static List<Stylist> GetAll()
+        {
+            List<Stylist> allStylists = new List<Stylist>{};
+
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM stylists;", conn);
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                string stylistName = rdr.GetString(0);
+                string stylistShift = rdr.GetString(1);
+                string stylistSpecialty = rdr.GetString(2);
+                int stylistId = rdr.GetInt32(3);
+
+                Stylist newStylist = new Stylist(stylistName, stylistShift, stylistSpecialty, stylistId);
+                AllStylists.Add(newStylist);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+            return allStylists;
+        }
+
+        //    save method for stylists
+        public void Save()
+        {
+            SqlConnection conn = DB.Connection();
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO stylists (name, shift, specialty) OUTPUT INSERTED.id VALUES (@StylistName, @StylistShift, @StylistSpecialty);", conn);
+
+            SqlParameter nameParameter = new SqlParameter();
+            nameParameter.ParameterName = "@StylistName";
+            nameParameter.Value = this.GetName();
+
+            SqlParameter shiftParameter = new SqlParameter();
+            shiftParameter.ParameterName = "@StylistShift";
+            shiftParameter.Value = this.GetShift();
+
+            SqlParameter specialtyParameter = new SqlParameter();
+            specialtyParameter.ParameterName = "@StylistSpecialty";
+            specialtyParameter.Value = this.GetSpecialty();
+
+            cmd.Parameters.Add(nameParameter);
+            cmd.Parameters.Add(shiftParameter);
+            cmd.Parameters.Add(specialtyParameter);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while(rdr.Read())
+            {
+                this._id = rdr.GetInt32(0);
+            }
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+
         // method to run multiple tests at once
         public static void DeleteAll()
         {
